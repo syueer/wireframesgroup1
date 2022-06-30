cd0407c95fec58682f05d93924d964d750a2c888
syueer <syue531@hotmail.com>
2022-06-30 15:07:24 +1000

    add task

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let tasks = new TaskManager()
+
 const todaysDate = new Date();
 const day = todaysDate.getDate();
 // const thisMonth = todaysDate.getMonth()+1;
@@ -5,38 +7,38 @@ const months = ["January", "February", "March", "April", "May", "June", "July", 
 const thisMonth = months[todaysDate.getMonth()];
 const thisYear = todaysDate.getFullYear();
 
-document.getElementById('dateDisplay').innerText= `Today is ${day}  ${thisMonth}  ${thisYear}.`;
+document.getElementById('dateDisplay').innerText = `Today is ${day}  ${thisMonth}  ${thisYear}.`;
 
 //Name validation
 
 const taskInput = document.getElementById('taskname');
 //const nameValue = document.getElementById('taskname').value;
 const validateName = () => {
-    
-    if(taskInput.value.length === 0) {
-        taskInput.style.border = '1px solid red';
-    } else if (taskInput.value.length < 8) {
-        document.getElementById('taskNameErr').style.display = 'block';
-        taskInput.style.border = '1px solid red';
-    } else {
-        document.getElementById('taskNameErr').style.display = 'none';
-        taskInput.style.border = '1px solid #ced4da';
-    }
+
+  if (taskInput.value.length === 0) {
+    taskInput.style.border = '1px solid red';
+  } else if (taskInput.value.length < 8) {
+    document.getElementById('taskNameErr').style.display = 'block';
+    taskInput.style.border = '1px solid red';
+  } else {
+    document.getElementById('taskNameErr').style.display = 'none';
+    taskInput.style.border = '1px solid #ced4da';
+  }
 }
 taskInput.addEventListener('blur', validateName);
 
 //Description validation
 const descriptionInput = document.getElementById('taskdescription');
 const validateDescription = () => {
-    if(descriptionInput.value.length === 0) {
-        descriptionInput.style.border = '1px solid red';
-    } else if (descriptionInput.value.length > 15) {
-        document.getElementById('taskDescriptionErr').style.display = 'block';
-        descriptionInput.style.border = '1px solid red';
-    } else {
-        document.getElementById('taskDescriptionErr').style.display = 'none';
-        descriptionInput.style.border = '1px solid #CED4DA';
-    }
+  if (descriptionInput.value.length === 0) {
+    descriptionInput.style.border = '1px solid red';
+  } else if (descriptionInput.value.length > 15) {
+    document.getElementById('taskDescriptionErr').style.display = 'block';
+    descriptionInput.style.border = '1px solid red';
+  } else {
+    document.getElementById('taskDescriptionErr').style.display = 'none';
+    descriptionInput.style.border = '1px solid #CED4DA';
+  }
 }
 descriptionInput.addEventListener('blur', validateDescription);
 
@@ -69,39 +71,53 @@ const error = document.getElementById('submitErr');
 
 error.style.display = 'block';
 error.style.visibility = 'hidden';
-const validateSubmit = () => {
-    if (taskInput.value === '' && descriptionInput.value === '' && taskDate.value === '') {
-        taskInput.style.border = '1px solid red';
-        descriptionInput.style.border = '1px solid red';
-        taskDate.style.border = '1px solid red';
-        error.style.visibility = 'visible';
-        return false;
-   } else if (descriptionInput.value === '' && taskDate.value === ''){
+
+const assigneeInput = document.getElementById('assignee');
+const statusInput = document.getElementById('status');
+
+const validationForm = () => {
+  if (taskInput.value === '' && descriptionInput.value === '' && taskDate.value === '') {
+    taskInput.style.border = '1px solid red';
     descriptionInput.style.border = '1px solid red';
-        taskDate.style.border = '1px solid red';
-        error.style.visibility = 'visible';
-      
-   } else if (taskInput.value === '' && descriptionInput.value === ''){
-        taskInput.style.border = '1px solid red';
-        descriptionInput.style.border = '1px solid red';
-        error.style.visibility = 'visible';
-       
-   } else if (taskInput.value === '' && taskDate.value === ''){
-        taskInput.style.border = '1px solid red';
-        error.style.visibility = 'visible';
-   } else if (descriptionInput.value === '') {
+    taskDate.style.border = '1px solid red';
+    error.style.visibility = 'visible';
+    return false;
+  } else if (descriptionInput.value === '' && taskDate.value === '') {
     descriptionInput.style.border = '1px solid red';
-        error.style.visibility = 'visible';
-   } else if (taskInput.value === '') {
-        taskInput.style.border = '1px solid red';
-        error.style.visibility = 'visible';
-   } else if (taskDate.value === '') {
-        taskDate.style.border = '1px solid red';
-        error.style.visibility = 'visible';
-   }
-   else {
-        document.getElementById('submitConfirm').style.display = 'block';
-        return true;
-   }    
- }
- 
+    taskDate.style.border = '1px solid red';
+    error.style.visibility = 'visible';
+
+  } else if (taskInput.value === '' && descriptionInput.value === '') {
+    taskInput.style.border = '1px solid red';
+    descriptionInput.style.border = '1px solid red';
+    error.style.visibility = 'visible';
+
+  } else if (taskInput.value === '' && taskDate.value === '') {
+    taskInput.style.border = '1px solid red';
+    error.style.visibility = 'visible';
+  } else if (descriptionInput.value === '') {
+    descriptionInput.style.border = '1px solid red';
+    error.style.visibility = 'visible';
+  } else if (taskInput.value === '') {
+    taskInput.style.border = '1px solid red';
+    error.style.visibility = 'visible';
+  } else if (taskDate.value === '') {
+    taskDate.style.border = '1px solid red';
+    error.style.visibility = 'visible';
+  }
+  else {
+    document.getElementById('submitConfirm').style.display = 'block';
+    return true;
+  }
+}
+const taskList = document.getElementById('taskList')
+const submit = () => {
+  if (validationForm()) {
+    tasks.addTask(taskInput.value, descriptionInput.value, assigneeInput.value, taskDate.value, statusInput.value)
+    console.log(tasks)
+    tasks.render()
+    taskInput.value = ''
+    descriptionInput.value = ''
+    taskDate.value = ''
+  }
+}
---
+++ taskManager.js
@@ -0,0 +1,62 @@
+
+const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
+  const taskHtml = `
+  <div class="col-sm-6">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Task</h5>
+      <p class="card-text">
+        Task Name: ${name}
+        <br />
+        Description: ${description}
+        <br />
+        Assignee: ${assignedTo}
+        <br />
+        Date: ${dueDate}
+        <br />
+        Status: ${status}
+        <br />
+        Priority: Moderate
+      </p>
+      <a
+        href="#"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        >Edit</a
+      >
+      <a href="#" class="btn btn-primary">Delete</a>
+    </div>
+  </div>
+</div>
+  `
+  return taskHtml
+}
+
+
+
+
+class TaskManager {
+  constructor(currentId = 0) {
+    this.currentId = currentId
+    this.tasks = []
+  }
+
+  addTask(curname, curdescription, curassignedTo, curdueDate, curstatus = 'todo') {
+    let taskObject = { currentId: this.currentId++, name: curname, description: curdescription, assignedTo: curassignedTo, dueDate: curdueDate, status: curstatus }
+    this.tasks.push(taskObject)
+  }
+
+  render() {
+    let tasksHtmlList = []
+    this.tasks.map(task => {
+      let currentTask = task;
+      let date = new Date(currentTask.dueDate)
+      let formattedDate = (date.toLocaleDateString())
+      let taskHtml = createTaskHtml(currentTask.name, currentTask.description, currentTask.assignedTo, formattedDate, currentTask.status)
+      tasksHtmlList.push(taskHtml)
+    })
+    let tasksHtml = tasksHtmlList.join('\n')
+    taskList.innerHTML = tasksHtml
+  }
+}
